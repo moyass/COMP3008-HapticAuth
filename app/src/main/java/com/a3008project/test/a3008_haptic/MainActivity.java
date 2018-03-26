742998d8db88c7c95ef1b84565c1dd1010299e4d
Mohamad Yassine <mohamad.yassine@carleton.ca>
2018-03-26 11:33:03 -0400

    Comments in the code

--- app/src/main/java/com/a3008project/test/a3008_haptic/MainActivity.java
+++ app/src/main/java/com/a3008project/test/a3008_haptic/MainActivity.java
@@ -19,33 +19,48 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * DOCUMENTATION FOR VIBRATION
+ * https://developer.android.com/reference/android/os/VibrationEffect.html
+ * */
+
 public class MainActivity extends AppCompatActivity {
 
     Button one,two,three,four;
     Vibrator vibrator;
+    // TODO: Implement touch and hold input to include the time component.
 
-    // Store the input into this sequence like so
-    List<String> input_sequence;
+    /**
+    * Store the input as follows:
+    *   1. A,B,C,D for the selected box
+    *   2. Record time for each tap and convert it to dots '.'
+    *   3. Append them to input_sequence
+    *   4. dis is the earf
+    * */
 
-
-
+    List<String> input_sequence; // KEY1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Load an activity from res/layouts
         setContentView(R.layout.activity_main);
 
-
-
+        // Collect the elements from the activity and store them into objects
         one = findViewById(R.id.button1);
         two = findViewById(R.id.button2);
         three = findViewById(R.id.button3);
         four = findViewById(R.id.button4);
 
+        // Initialize the vibrator
         vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
+        // THE FOLLOWING ONLY WORKS FOR API 26 (Android 8.0)
+        // The testing device does not have API 26 and I don't think many do either
+        // I'll keep this here until one of ya'll deletes it xD
         //vibrator.hasAmplitudeControl();
 
-        Parcelable.Creator<VibrationEffect> CREATOR;
 
 
         one.setOnClickListener(new View.OnClickListener() {
@@ -90,12 +105,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-
                 long [] test = {200,100,150,200};
                 vibrator.vibrate(test, -1);
-
-
-
             }
         });
 
